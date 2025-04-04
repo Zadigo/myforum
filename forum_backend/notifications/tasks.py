3ce1b74dc2b377444d0304aff3ef2f8ca7e763ca
Zadigo <pendenquejohn@gmail.com>
2025-04-04 14:49:08 +0200

    Improve tasks

--- forum_backend/notifications/tasks.py
+++ forum_backend/notifications/tasks.py
@@ -3,6 +3,7 @@ from celery.utils.log import get_logger
 from comments.models import Comment
 from django.contrib.auth import get_user_model
 from notifications.models import Notification
+from threads.models import MainThread
 
 logger = get_logger('comments')
 
@@ -24,7 +25,7 @@ def create_notifications(**kwargs: dict[str, str | int]):
     users = get_user_model().objects.filter(id__in=user_ids)
     # Only notify active users -; banned users
     # however can still be mentionned or notified
-    active_users = users.filte(active=True)
+    active_users = users.filter(is_active=True)
 
     for user in active_users:
         Notification.objects.create(**{
@@ -44,3 +45,20 @@ def create_message_notifications(comment_id: int, user_ids: list[int]):
         'notification_type': 'Message'
     })
     return user_ids
+
+
+@shared_task
+def create_thread_followers_notifications(thread_id: int):
+    """Notify the users following a given thread that an
+    event has occured in a thread"""
+    try:
+        instance = MainThread.objects.get(id=thread_id)
+    except:
+        logger.error('Thread does not exist')
+        return
+    else:
+        user_ids = instance.followers.values_list('id', flat=True)
+        create_notifications.apply_async(kwargs={
+            'user_ids': list(user_ids),
+            'notification_type': 'Message'
+        })
